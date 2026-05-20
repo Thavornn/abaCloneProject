5055977267292c96d495f6c307970f883b2439a4
vorn <vorn@example.com>
2026-05-20 18:03:57 +0700

    add featureSplit page

--- src/components/FeatureSpilt.tsx
+++ src/components/FeatureSpilt.tsx
@@ -2,19 +2,19 @@ import { featuresData } from "../data/MultiplePage";
 
 export default function FeatureSpilt() {
   return (
-    <div className="w-full py-16 space-y-24 font-raleway ">
+    <div className="w-full max-w-screen-2xl mx-auto py-16 space-y-32 px-6 font-raleway ">
       {featuresData.map((feature, index) => {
         const isOddRow = index % 2 == 1;
         return (
           <div className="flex justify-center flex-col md:flex-row items-center gap-12 ">
             {/* Text */}
             <div
-              className={`px-50 ${isOddRow ? "md:order-last" : "md:order-first"}`}
+              className={` ${isOddRow ? "md:order-first" : "md:order-last"}`}
             >
               <h2 className="font-bold text-[40px] text-gray">
                 {feature.title}
               </h2>
-              <p className="w-[700px]">{feature.description}</p>
+              <p className="w-[300px]">{feature.description}</p>
             </div>
 
             {/* Picture */}
