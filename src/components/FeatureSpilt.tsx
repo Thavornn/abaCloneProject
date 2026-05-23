113adc9e26793a5bb30bf215ea2ca3f20d03e6c6
vorn <vorn@example.com>
2026-05-23 14:36:37 +0700

    finish featuresplit page

--- src/components/FeatureSpilt.tsx
+++ src/components/FeatureSpilt.tsx
@@ -2,14 +2,14 @@ import { featuresData } from "../data/MultiplePage";
 
 export default function FeatureSpilt() {
   return (
-    <div className="w-full max-w-screen-2xl mx-auto py-16 space-y-32 px-6 font-raleway ">
+    <div className="w-full max-w-screen-2xl mx-auto py-16 space-y-32  font-raleway ">
       {featuresData.map((feature, index) => {
-        const isOddRow = index % 2 == 1;
+        const isEvenNum = index % 2 == 1;
         return (
-          <div className="flex justify-center flex-col md:flex-row items-center gap-12 ">
+          <div className="grid justify-items-center items-center grid-cols-1 md:grid-cols-2 ">
             {/* Text */}
             <div
-              className={` ${isOddRow ? "md:order-first" : "md:order-last"}`}
+              className={` ${isEvenNum ? "md:order-first" : "md:order-last"}`}
             >
               <h2 className="font-bold text-[40px] text-gray">
                 {feature.title}
@@ -18,7 +18,9 @@ export default function FeatureSpilt() {
             </div>
 
             {/* Picture */}
-            <div>
+            <div
+              className={` ${isEvenNum ? "md:order-last" : "md:order-first"}`}
+            >
               <img src={feature.imageSrc} className="" />
             </div>
           </div>
